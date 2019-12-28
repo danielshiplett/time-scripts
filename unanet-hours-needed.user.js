ab0d2749fce4025a72685c4cc2421573373528cf
Daniel Shiplett <danielshiplett@users.noreply.github.com>
2019-12-28 18:10:59 -0500

    Add More Info

--- unanet-hours-needed.user.js
+++ unanet-hours-needed.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Calculate the number of hours needed for the rest of the month.
-// @author       ddshipl
+// @author       Daniel Shiplett
 // @namespace    https://github.com/danielshiplett
 // @match        https://*.unanet.biz/*/action/time/edit*
 // @grant        none
@@ -22,20 +22,26 @@
     }
 })();
 
+/* Add a button with a function that calculates remaining hours to work. */
 (function() {
     'use strict';
 
     console.log("starting script");
 
     // Create a button and a disabled input.
-    var html = '<p class="sub-main"><button type="button" title="Calculate" id="calculate">Calculate</button>Hours / Day Remaining: <input id="hoursperdayleft" type="text" size="5" disabled></p>';
+    var html = '' +
+        '<p><button type="button" title="Calculate Remaining Hours" id="calculate" style="margin-bottom: 5px">Calculate Remaining Hours</button></p>' +
+        '<p>Hours Remaining: <input id="hoursleft" type="text" size="5" disabled></p>' +
+        '<p>Days Remaining: <input id="daysleft" type="text" size="5" disabled></p>' +
+        '<p>Hours / Day Remaining: <input id="hoursperdayleft" type="text" size="5" disabled></p>' +
+        '';
     var template = document.createElement('template');
     html = html.trim(); // Never return a text node of whitespace as the result
     template.innerHTML = html;
-    console.log(template.content.firstChild);
+    console.log(template.content);
 
     // Inject the new HTML right after the warning.
-    document.querySelector("#timeContent > div.warning > div").append(template.content.firstChild);
+    document.querySelector("#timeContent > div.warning > div").append(template.content);
 
     // Add our function to the button click.
     document.querySelector('#calculate').addEventListener('click', function() {
@@ -53,8 +59,8 @@
         console.log('warning hours: ' + warningHours[1]);
 
         // We can then parse and subtract to get the hours we need to complete.
-        var todoHours = parseFloat(warningHours[1]) - parseFloat(totalTime.value);
-        console.log('todoHours: ' + todoHours);
+        var hoursLeft = parseFloat(warningHours[1]) - parseFloat(totalTime.value);
+        console.log('hoursLeft: ' + hoursLeft);
 
         // Then, we can get the totals row.
         var totalRow = document.querySelector("#timesheet > tbody:nth-child(3) > tr");
@@ -68,11 +74,11 @@
 
         for (let cell of cells) {
             if(cell.className == 'weekday-totals') {
-                console.log("cell: " + cell.childNodes[0].value);
+                //console.log("cell: " + cell.childNodes[0].value);
                 var weekdayValue = cell.childNodes[0].value;
 
                 if(weekdayValue == '') {
-                    console.log("emptyDay");
+                    //console.log("emptyDay");
                     daysLeft += 1;
                 }
             }
@@ -81,10 +87,12 @@
         console.log("days left: " + daysLeft);
 
         // Now calculate how many hours per day we need to work for the rest of the month to meet our required hours.
-        var hoursPerDay = todoHours / daysLeft;
+        var hoursPerDay = hoursLeft / daysLeft;
         console.log("hours per day: " + hoursPerDay);
 
         // And insert the hours into the input text box.
+        document.getElementById('hoursleft').value = hoursLeft;
+        document.getElementById('daysleft').value = daysLeft;
         document.getElementById('hoursperdayleft').value = hoursPerDay;
     })
 })();
